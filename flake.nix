6773faa29a999e9ea6a7cae1fecc06ac921f5b1d
laksith19 <admin@laksith.dev>
2024-10-15 10:59:16 -0700

    restore home

--- flake.nix
+++ flake.nix
@@ -4,10 +4,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
 
-    # home-manager = {
-    #   url = "github:nix-community/home-manager";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     stylix.url = "github:danth/stylix/release-24.05";
   };
@@ -17,12 +17,12 @@
       specialArgs = {inherit inputs;};
       modules = [
         ./nixos/configuration.nix
-        # inputs.home-manager.nixosModules.default
-        # {
-        #   inputs.home-manager.useGlobalPkgs = true;
-        #   inputs.home-manager.useUserPackages = true;
-        #   inputs.home-manager.users.laksith = import ./home.nix;
-        # }
+        inputs.home-manager.nixosModules.default
+        {
+          inputs.home-manager.useGlobalPkgs = true;
+          inputs.home-manager.useUserPackages = true;
+          inputs.home-manager.users.laksith = import ./home.nix;
+        }
         inputs.stylix.nixosModules.stylix
       ];
     };
