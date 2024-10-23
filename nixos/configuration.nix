46d5b285ee8cc31ef39d291e3d5f9a530dd556be
laksith19 <admin@laksith.dev>
2024-10-22 17:52:47 -0700

    try to get wayland spotify

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -64,6 +64,16 @@
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   
+  # Wrap the spotify package to use wayland by default
+  pkgs.symlinkJoin {
+    name = "spotify";
+    paths = [ pkgs.spotify ];
+    buildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/spotify \
+        --add-flags "--enable-features=UseOzonePlatform --ozone-platform=wayland"
+    '';
+  }
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
