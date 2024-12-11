0ba42e470c38436ab7e963f96157da5ec784a8cd
laksith19 <admin@laksith.dev>
2024-12-10 17:23:09 -0800

    feat: add comma

--- flake.nix
+++ flake.nix
@@ -4,6 +4,12 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+
+    nix-index-database = {
+      url = "github:nix-community/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
 
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
@@ -15,7 +21,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
   outputs = {
@@ -25,6 +30,7 @@
     home-manager,
     stylix,
     nixos-hardware,
+    nix-index-database,
     ...
   }: let
     system = "x86_64-linux";
@@ -34,14 +40,23 @@
     nixosConfigurations.quirrel = nixpkgs.lib.nixosSystem {
       specialArgs = {inherit pkgs-unstable;};
       modules = [
+
         ./hosts/quirrel.nix
+
         nixos-hardware.nixosModules.lenovo-thinkpad-x13-amd
+
+        nix-index-database.nixosModules.nix-index
+        {
+          programs.nix-index-database.comma.enable = true;
+        }
+
         home-manager.nixosModules.default
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.users.laksith = import ./homes/laksith.nix;
         }
+
         stylix.nixosModules.stylix
       ];
     };
