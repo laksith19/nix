37df736ce584ebf2b840b0c541632f5b941f369b
laksith19 <admin@laksith.dev>
2025-03-16 07:25:57 -0700

    optimise nix and configure automatic garbage collection

--- hosts/quirrel.nix
+++ hosts/quirrel.nix
@@ -9,7 +9,15 @@
 
   nix = {
     channel.enable = false;
-    settings.experimental-features = ["nix-command" "flakes"];
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
+    settings = {
+      experimental-features = ["nix-command" "flakes"];
+      auto-optimise-store = true;
+    };
   };
 
   nixpkgs.config.allowUnfree = true;
